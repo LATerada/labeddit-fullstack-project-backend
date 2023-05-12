4f073c829e4aab186185f00429bb407203d2e01a
LATerada <larissaakiterada@gmail.com>
2023-05-12 13:01:07 -0300

    :construction: Add getPosts.dto

---
+++ src/dtos/post/getPosts.dto.ts
@@ -0,0 +1,10 @@
+import z from "zod";
+import { PostModel } from "../../models/Post";
+
+export const GetPostsSchema = z.object({
+  token: z.string().min(1),
+});
+
+export type GetPostsInputDTO = z.infer<typeof GetPostsSchema>;
+
+export type GetPostsOutputDTO = PostModel[];
