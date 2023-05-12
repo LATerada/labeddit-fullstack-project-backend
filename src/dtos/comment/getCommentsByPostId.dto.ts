c6fd67e7a2fb9355319c96ba9fa01b1519b6daeb
LATerada <larissaakiterada@gmail.com>
2023-05-12 13:17:25 -0300

    :construction: Add getCommentsByPosId.dto

---
+++ src/dtos/comment/getCommentsByPostId.dto.ts
@@ -0,0 +1,11 @@
+import z from "zod";
+import { CommentModel } from "../../models/Comment";
+
+export const GetCommentsSchema = z.object({
+  token: z.string().min(1),
+  post_id: z.string().min(1),
+});
+
+export type GetCommentsInputDTO = z.infer<typeof GetCommentsSchema>;
+
+export type GetCommentsOutputDTO = CommentModel[];
