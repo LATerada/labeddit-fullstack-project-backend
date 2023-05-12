57734e1ec16703eccd5c6d1436052c21d7dac853
LATerada <larissaakiterada@gmail.com>
2023-05-12 13:18:46 -0300

    :construction: Add likeOrDislikeComment.dto

---
+++ src/dtos/comment/likeOrDislikeComment.dto.ts
@@ -0,0 +1,11 @@
+import z from "zod";
+
+export const LikeOrDislikeCommentSchema = z.object({
+  token: z.string().min(1),
+  idToLikeOrDislike: z.string().min(1),
+  like: z.boolean(),
+});
+
+export type LikeOrDislikeCommentInputDTO = z.infer<typeof LikeOrDislikeCommentSchema>;
+
+export type LikeOrDislikeCommentOutputDTO = undefined;
