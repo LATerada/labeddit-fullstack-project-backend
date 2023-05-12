77e53a298f8a42529ed8d44d10cd6cff9bdff61f
LATerada <larissaakiterada@gmail.com>
2023-05-12 13:08:51 -0300

    :construction: Add createComment.dto

---
+++ src/dtos/comment/createComment.dto.ts
@@ -0,0 +1,11 @@
+import z from "zod";
+
+export const CreateCommentSchema = z.object({
+  token: z.string().min(1),
+  post_id: z.string().min(1),
+  content: z.string().min(1),
+});
+
+export type CreateCommentInputDTO = z.infer<typeof CreateCommentSchema>;
+
+export type CreateCommentOutputDTO = undefined;
