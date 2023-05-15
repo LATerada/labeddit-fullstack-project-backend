94a5de84674eee7cc143b87cadda38338d8256d6
LATerada <larissaakiterada@gmail.com>
2023-05-15 16:18:11 -0300

    :sparkles: Add createPost endpoint

--- src/database/PostDatabase.ts
+++ src/database/PostDatabase.ts
@@ -1,5 +1,10 @@
+import { PostDB } from "../models/Post";
 import { BaseDatabase } from "./BaseDatabase";
 
 export class PostDatabase extends BaseDatabase {
   public static TABLE_POSTS = "posts";
+
+  public insertPost = async (newPost: PostDB) => {
+    await BaseDatabase.connection(PostDatabase.TABLE_POSTS).insert(newPost);
+  };
 }
