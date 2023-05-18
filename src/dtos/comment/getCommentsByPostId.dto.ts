639f11d04f925999ab7b4ddbc2f6ea357902772b
LATerada <larissaakiterada@gmail.com>
2023-05-18 19:17:31 -0300

    :sparkles: Add getCommentsByPostId

--- src/dtos/comment/getCommentsByPostId.dto.ts
+++ src/dtos/comment/getCommentsByPostId.dto.ts
@@ -1,13 +1,13 @@
 import z from "zod";
-import { CommentModel } from "../../models/Comment";
+import { PostCommentModel } from "../../models/Comment";
 
 export const GetCommentsSchema = z.object({
   token: z.string().min(1),
-  post_id: z.string().min(1),
+  postId: z.string().min(1),
 });
 
 export type GetCommentsInputDTO = z.infer<typeof GetCommentsSchema>;
 
 export interface GetCommentsOutputDTO {
-  comments: CommentModel[];
+  comments: PostCommentModel[];
 }
