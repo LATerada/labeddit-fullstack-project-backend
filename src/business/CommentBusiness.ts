9ce9ebaba009c86870cb42abcc28ac7257487d13
LATerada <larissaakiterada@gmail.com>
2023-05-18 17:27:59 -0300

    :sparkles: Add createComment endpoint

--- src/business/CommentBusiness.ts
+++ src/business/CommentBusiness.ts
@@ -1,11 +1,71 @@
+import { NotBeforeError } from "jsonwebtoken";
 import { CommentDatabase } from "../database/CommentDatabase";
+import { PostDatabase } from "../database/PostDatabase";
+import {
+  CreateCommentInputDTO,
+  CreateCommentOutputDTO,
+} from "../dtos/comment/createComment.dto";
+import { ForbiddenError } from "../errors/ForbiddenError";
+import { NotFoundError } from "../errors/NotFoundError";
+import { UnauthorizedError } from "../errors/UnaouthorizedError";
+import { Comment, PostCommentDB } from "../models/Comment";
 import { IdGenerator } from "../services/IdGenerator";
 import { TokenManager } from "../services/TokenManeger";
 
 export class CommentBusiness {
   constructor(
-    private CommentDatabase: CommentDatabase,
+    private commentDatabase: CommentDatabase,
+    private postDatabase: PostDatabase,
     private idGenerator: IdGenerator,
     private tokenManeger: TokenManager
   ) {}
+
+  public createComment = async (
+    input: CreateCommentInputDTO
+  ): Promise<CreateCommentOutputDTO> => {
+    const { token, postId, content } = input;
+
+    const payload = this.tokenManeger.getPayload(token);
+
+    if (!payload) {
+      throw new UnauthorizedError("Invalid token");
+    }
+
+    const postIdExists = await this.postDatabase.findPostsWithCreatorNameById(
+      postId
+    );
+
+    if (!postIdExists) {
+      throw new NotFoundError("Invalid post id");
+    }
+
+    const id = this.idGenerator.generate();
+
+    const newComment = new Comment(
+      id,
+      content,
+      0,
+      0,
+      new Date().toISOString(),
+      payload.id,
+      payload.name
+    );
+
+    const newCommentDB = newComment.toDBModel();
+
+    await this.commentDatabase.insertComment(newCommentDB);
+
+    const newPostCommentDB: PostCommentDB = {
+      post_id: postId,
+      comment_id: id,
+    };
+
+    await this.commentDatabase.insertPostComment(newPostCommentDB);
+
+    const output: CreateCommentOutputDTO = {
+      message: "Comment created",
+    };
+
+    return output;
+  };
 }
--- src/routes/commentRouter.ts
+++ src/routes/commentRouter.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { CommentBusiness } from "../business/CommentBusiness";
 import { CommentController } from "../controller/CommentController";
 import { CommentDatabase } from "../database/CommentDatabase";
+import { PostDatabase } from "../database/PostDatabase";
 import { IdGenerator } from "../services/IdGenerator";
 import { TokenManager } from "../services/TokenManeger";
 
@@ -10,7 +11,10 @@ export const commentRouter = express.Router();
 const commentController = new CommentController(
   new CommentBusiness(
     new CommentDatabase(),
+    new PostDatabase(),
     new IdGenerator(),
     new TokenManager()
   )
 );
+
+commentRouter.post("/:id", commentController.createComment);
