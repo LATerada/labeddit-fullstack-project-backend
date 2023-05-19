708718f253e7c9a1ec962197534e0cabec677a63
LATerada <larissaakiterada@gmail.com>
2023-05-19 11:50:48 -0300

    :white_check_mark: Add signup test

---
+++ tests/business/user/signup.test.ts
@@ -0,0 +1,29 @@
+import { UserBusiness } from "../../../src/business/UserBusiness";
+import { SignupSchema } from "../../../src/dtos/user/signup.dto";
+import { UserDatabaseMock } from "../../mocks/UserDatabaseMock";
+import { IdGeneratorMock } from "../../mocks/IdGeneratorMock";
+import { TokenManagerMock } from "../../mocks/TokenManegerMock";
+import { HashManagerMock } from "../../mocks/HashManegerMock";
+
+describe("Signup tests", () => {
+  const userBusiness = new UserBusiness(
+    new UserDatabaseMock(),
+    new IdGeneratorMock(),
+    new TokenManagerMock(),
+    new HashManagerMock()
+  );
+
+  test("Must generate token when registering", async () => {
+    const input = SignupSchema.parse({
+      name: "User2",
+      email: "user2@email.com",
+      password: "user2023",
+    });
+
+    const output = await userBusiness.signup(input);
+
+    expect(output).toEqual({
+      token: "token-mock",
+    });
+  });
+});
