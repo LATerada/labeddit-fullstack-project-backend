eecdef209b79c6e69e469ed02e11bdc140e79289
LATerada <larissaakiterada@gmail.com>
2023-05-20 18:14:20 -0300

    :goal_net: Add error test to createComment

--- tests/business/comment/createComment.test.ts
+++ tests/business/comment/createComment.test.ts
@@ -4,6 +4,8 @@ import { IdGeneratorMock } from "../../mocks/IdGeneratorMock";
 import { TokenManagerMock } from "../../mocks/TokenManagerMock";
 import { CommentDatabaseMock } from "../../mocks/CommentDatabaseMock";
 import { CreateCommentSchema } from "../../../src/dtos/comment/createComment.dto";
+import { UnauthorizedError } from "../../../src/errors/UnaouthorizedError";
+import { NotFoundError } from "../../../src/errors/NotFoundError";
 
 describe("CreateComment tests", () => {
   const commentBusiness = new CommentBusiness(
@@ -17,7 +19,8 @@ describe("CreateComment tests", () => {
     const input = CreateCommentSchema.parse({
       token: "token-mock-normal",
       postId: "post-id-mock2",
-      content: "I saw that many teachers are using it more often than their students are.",
+      content:
+        "I saw that many teachers are using it more often than their students are.",
     });
 
     const output = await commentBusiness.createComment(input);
@@ -25,5 +28,41 @@ describe("CreateComment tests", () => {
     expect(output).toEqual({
       message: "Comment created",
     });
+  });
+
+  test("Must return error when token is invalid", async () => {
+    expect.assertions(2);
+    try {
+      const input = CreateCommentSchema.parse({
+        token: "invalid-token",
+        postId: "post-id-mock2",
+        content: "Hello World!",
+      });
+
+      const output = await commentBusiness.createComment(input);
+    } catch (error) {
+      if (error instanceof UnauthorizedError) {
+        expect(error.statusCode).toBe(401);
+        expect(error.message).toBe("Invalid token");
+      }
+    }
+  });
+
+  test("Must return error when post id doesn't exist", async () => {
+    expect.assertions(2);
+    try {
+      const input = CreateCommentSchema.parse({
+        token: "token-mock-normal",
+        postId: "invalid-post-id",
+        content: "Hello World!",
+      });
+
+      const output = await commentBusiness.createComment(input);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        expect(error.statusCode).toBe(404);
+        expect(error.message).toBe("Invalid post id");
+      }
+    }
   });
 });
