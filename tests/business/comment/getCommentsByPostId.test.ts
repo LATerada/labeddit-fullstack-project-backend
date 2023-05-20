0d274be9c671834449505457dff30f4eb99c1c01
LATerada <larissaakiterada@gmail.com>
2023-05-20 18:17:43 -0300

    :goal_net: Add error test to getCommentByPostId

--- tests/business/comment/getCommentsByPostId.test.ts
+++ tests/business/comment/getCommentsByPostId.test.ts
@@ -4,6 +4,8 @@ import { IdGeneratorMock } from "../../mocks/IdGeneratorMock";
 import { TokenManagerMock } from "../../mocks/TokenManagerMock";
 import { CommentDatabaseMock } from "../../mocks/CommentDatabaseMock";
 import { GetCommentsSchema } from "../../../src/dtos/comment/getCommentsByPostId.dto";
+import { UnauthorizedError } from "../../../src/errors/UnaouthorizedError";
+import { NotFoundError } from "../../../src/errors/NotFoundError";
 
 describe("GetCommentByPostId tests", () => {
   const commentBusiness = new CommentBusiness(
@@ -43,5 +45,39 @@ describe("GetCommentByPostId tests", () => {
         },
       ],
     });
+  });
+
+  test("Must return error when token is invalid", async () => {
+    expect.assertions(2);
+    try {
+      const input = GetCommentsSchema.parse({
+        token: "invalid-token",
+        postId: "post-id-mock2",
+      });
+
+      const output = await commentBusiness.getCommentsByPostId(input);
+    } catch (error) {
+      if (error instanceof UnauthorizedError) {
+        expect(error.statusCode).toBe(401);
+        expect(error.message).toBe("Invalid token");
+      }
+    }
+  });
+
+  test("Must return error when post id doesn't exist", async () => {
+    expect.assertions(2);
+    try {
+      const input = GetCommentsSchema.parse({
+        token: "token-mock-normal",
+        postId: "invalid-post-id",
+      });
+
+      const output = await commentBusiness.getCommentsByPostId(input);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        expect(error.statusCode).toBe(404);
+        expect(error.message).toBe("Invalid post id");
+      }
+    }
   });
 });
