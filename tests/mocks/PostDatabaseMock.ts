adee3ae23e9e7151950dce4ee171a26c969af6c4
LATerada <larissaakiterada@gmail.com>
2023-05-20 18:00:19 -0300

    :coffin: Remove dead mock

--- tests/mocks/PostDatabaseMock.ts
+++ tests/mocks/PostDatabaseMock.ts
@@ -3,36 +3,8 @@ import {
   LikeDislikePostDB,
   PostDB,
   PostDBWithCreatorName,
-  PostModel,
   POST_LIKE,
 } from "../../src/models/Post";
-
-const postModelMock: PostModel[] = [
-  {
-    id: "post-id-mock",
-    postContent: "Hello World!",
-    likes: 0,
-    dislikes: 1,
-    comments: 0,
-    createdAt: "2023-05-19T11:55:00.924Z",
-    creator: {
-      id: "id-mock-normal",
-      name: "User",
-    },
-  },
-  {
-    id: "post-id-mock2",
-    postContent: "What do you think about ChatGPT?",
-    likes: 1,
-    dislikes: 0,
-    comments: 0,
-    createdAt: "2023-05-19T11:59:43.426Z",
-    creator: {
-      id: "id-mock-admin",
-      name: "Admin",
-    },
-  },
-];
 
 const postDBMock: PostDBWithCreatorName[] = [
   {
